e2009c63914323eb248635bc5cd199a4f082e8cf
Sara Piette <sepiette@gmail.com>
2020-06-02 15:41:58 -0400

    remove app.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.scss';
 import './styles/common.scss';
 import Container from '@material-ui/core/Container';
 import { EmailForm } from './components/EmailForm';
